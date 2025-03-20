7488cb725779ee8de5ef9a7c247ea011f3dde091
Christian van der Loo <chris.vanderloo@icloud.com>
2025-03-20 10:49:01 -0400

    sudo mv

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 cp -f com.chrisvander.themeswitch.plist ~/Library/LaunchAgents/com.chrisvander.themeswitch.plist
 sudo cp -f themeswitch.sh /usr/local/bin/themeswitch
 swiftc themeswitchd.swift -o themeswitchd
-sudo cp -f themeswitchd /usr/local/bin/themeswitchd
+sudo mv -f themeswitchd /usr/local/bin/themeswitchd
 launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/com.chrisvander.themeswitch.plist
 launchctl start com.chrisvander.themeswitch
