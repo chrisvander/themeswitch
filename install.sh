e16f55e7eef38128cb26d83096c9b7e0d0e77a8b
Christian van der Loo <chris.vanderloo@icloud.com>
2025-03-20 10:48:18 -0400

    fix install, use /bin/sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ln -sf com.chrisvander.themeswitch.plist ~/Library/LaunchAgents/com.chrisvander.themeswitch.plist
-sudo ln -sf themeswitch.sh /usr/local/bin/themeswitch.sh
+cp -f com.chrisvander.themeswitch.plist ~/Library/LaunchAgents/com.chrisvander.themeswitch.plist
+sudo cp -f themeswitch.sh /usr/local/bin/themeswitch
 swiftc themeswitchd.swift -o themeswitchd
-sudo ln -sf themeswitchd /usr/local/bin/themeswitchd
-launchctl bootstrap -w ~/Library/LaunchAgents/com.chrisvander.themeswitch.plist &> /dev/null
+sudo cp -f themeswitchd /usr/local/bin/themeswitchd
+launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/com.chrisvander.themeswitch.plist
 launchctl start com.chrisvander.themeswitch
--- themeswitch.sh
+++ themeswitch.sh
@@ -4,10 +4,10 @@ THEME=$1
 
 if [[ "$THEME" == 0 ]]; then
 	# dark
-	
+	ln -sf $HOME/.config/helix/themes/catppuccin_mocha_transparent.toml $HOME/.config/helix/themes/catppuccin_dynamic_transparent.toml
 else
 	# light
-	
+  ln -sf $HOME/.config/helix/themes/catppuccin_latte_transparent.toml $HOME/.config/helix/themes/catppuccin_dynamic_transparent.toml
 fi
 
 killall -SIGUSR1 hx
