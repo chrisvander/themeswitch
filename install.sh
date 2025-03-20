b73c60725d1fd425a54172e6257bcf38227f0ce0
Christian van der Loo <chris.vanderloo@icloud.com>
2025-03-20 09:45:43 -0400

    adjust scripts and plist

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+ln -sf com.chrisvander.themeswitch.plist ~/Library/LaunchAgents/com.chrisvander.themeswitch.plist
+sudo ln -sf themeswitch.sh /usr/local/bin/themeswitch.sh
 swiftc themeswitchd.swift -o themeswitchd
-cp com.tim-xie.themeswitch.plist ~/Library/LaunchAgents
-sudo cp themeswitch themeswitchd /usr/local/bin/
-launchctl load -w ~/Library/LaunchAgents/com.tim-xie.themeswitch.plist
-launchctl start com.tim-xie.themeswitch
+sudo ln -sf themeswitchd /usr/local/bin/themeswitchd
+launchctl bootstrap -w ~/Library/LaunchAgents/com.chrisvander.themeswitch.plist &> /dev/null
+launchctl start com.chrisvander.themeswitch
--- themeswitch.sh
+++ themeswitch.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
 THEME=$1
-BASE=/Users/pdt/.config/nvim
-
-# 0 = switch to dark
-# 1 = switch to light
 
 if [[ "$THEME" == 0 ]]; then
-	/opt/homebrew/bin/kitten themes --reload-in=all blazer-dark &> /tmp/log
-	cp "$BASE/colors/blazer-dark.lua" "${BASE}/colors/blazer.lua"
-	cp "$BASE/lua/lualine/themes/blazer-dark.lua" "${BASE}/lua/lualine/themes/blazer.lua"
+	# dark
+	
 else
-	/opt/homebrew/bin/kitten themes --reload-in=all blazer-light &> /tmp/log
-	cp "$BASE/colors/blazer-light.lua" "$BASE/colors/blazer.lua"
-	cp "$BASE/lua/lualine/themes/blazer-light.lua" "$BASE/lua/lualine/themes/blazer.lua"
+	# light
+	
 fi
 
-killall -SIGUSR1 nvim
+killall -SIGUSR1 hx
